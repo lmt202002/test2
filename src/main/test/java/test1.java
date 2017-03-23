a3fc79c12086b6b354a6da6cad7fbe52091723fb
lvshr <lvshr@vip.qq.com>
2017-03-23 13:21:03 +0800

    new

--- src/main/test/java/test1.java
+++ src/main/test/java/test1.java
@@ -1,5 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Created by lvshr on 2017/3/23.
  */
+
 public class test1 {
+    public static void main(String[] args ){
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://www.baidu.com");
+        WebElement searchbox=driver.findElement(By.id("kw"));
+        searchbox.sendKeys("selenium java");
+        WebElement searchButton=driver.findElement(By.id("su"));
+        searchButton.submit();
+
+        driver.close();
+    }
 }
