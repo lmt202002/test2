affffdef88372b1e90f451507d5563aed8a19de4
lvshr <lvshr@vip.qq.com>
2017-03-28 15:50:02 +0800

    new

---
+++ src/main/test/java/ForEachExample.java
@@ -0,0 +1,11 @@
+/**
+ * Created by lvshr on 2017/3/28.
+ */
+public class ForEachExample  {
+    public static void main(String[] args) {
+        int arr[] = { 12, 23, 44, 56, 78 };
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
--- src/main/test/java/test1.java
+++ src/main/test/java/test1.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 /**
  * Created by lvshr on 2017/3/23.
@@ -9,13 +11,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test1 {
     public static void main(String[] args ){
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver=new FirefoxDriver();
         driver.get("http://www.baidu.com");
         WebElement searchbox=driver.findElement(By.id("kw"));
         searchbox.sendKeys("selenium java");
         WebElement searchButton=driver.findElement(By.id("su"));
         searchButton.submit();
+        System.out.print("lallllllllll");
 
-        driver.close();
+//        driver.close();
     }
 }
